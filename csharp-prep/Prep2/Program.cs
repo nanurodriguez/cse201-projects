cca0c88aac776ce2cd2ac7c646c912258c403465
nanurodriguez <bel17039@byui.edu>
2024-09-20 15:22:49 -0600

    Completed the Prep 2 assignment- Grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade Percentage? ");
+        string userInput = Console.ReadLine();
+        float grade = float.Parse(userInput);
+
+        if (grade < 60)
+        {
+            Console.WriteLine("You have an F and have failed the class, I am so sorry!! :( ");
+        }
+        else if (grade > 60 && grade < 70 || grade == 60)
+        {
+            Console.WriteLine("You have a D. :o Please you can do better than this");
+        }
+        else if (grade > 70 && grade < 80 || grade == 70)
+        {
+            Console.WriteLine("You have a C");
+            Console.WriteLine("You passed!!");
+        }
+        else if (grade > 80 && grade < 90 || grade == 80)
+        {
+            Console.WriteLine("You have a B. WELL DONE!");
+        }
+        else
+        {
+            Console.WriteLine("Wait What??? You got an A!! You are AWESOME!!!");
+        }
     }
 }

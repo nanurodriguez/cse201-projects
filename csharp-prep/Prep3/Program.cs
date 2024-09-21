a8502b321e9b35e19277566d3700e3f22c36a679
nanurodriguez <bel17039@byui.edu>
2024-09-21 00:58:32 -0600

    working on Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?");
+        string userInputmagic = Console.ReadLine();
+        float magicNumber = float.Parse(userInputmagic);
+
+        Console.Write("What is your guess?");
+        string userInput = Console.ReadLine();
+        float guessNumber = float.Parse(userInput);
+
+
     }
 }

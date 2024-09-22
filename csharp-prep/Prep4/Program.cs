7f82c0f8c67288012949afaaf89f02acf3d5d57d
nanurodriguez <bel17039@byui.edu>
2024-09-21 19:47:46 -0600

    Adding the while loop and for loop for the Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Globalization;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> listNumbers = new List<int>();
+        Console.WriteLine("Hello There! Please enter a list of numbers, type 0 when you are done.");
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number:");
+            string userInput = Console.ReadLine();
+            int inputNumber = int.Parse(userInput);
+            if (inputNumber != 0)
+            {
+                listNumbers.Add(inputNumber);
+            }
+        }
+        //Now let's get the summary of those numbers that the user input.
+        int sum = 0; //Declaring the variable here to call it later.
+
+        foreach (int n in listNumbers)//foreach loop is the best way to go through a list and get their values.
+        {
+            sum += n; // sum of the numbers inside the list
+        }
+        Console.WriteLine($"The sum of those numbers you added is: {sum}");
+
+        /*Let's compute the average!*/
+
+        float average = ((float)sum) / listNumbers.Count;
+        Console.WriteLine($"The Average of those numbers you added is: {average}");
+
     }
+
 }

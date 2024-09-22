5fe2b94d7ba454e80ce757bd86e097ca1a2cddf6
nanurodriguez <bel17039@byui.edu>
2024-09-21 22:11:20 -0600

    Finishing P5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome(); //First Function
+        string userName = PromptUserName();//Second Function
+
+        int userNumber = PromptUserNumber();//Third Function
+
+        int squaredNumber = SquareNumber(userNumber);//Forth Function
+
+        DisplayResult(userName, squaredNumber);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.Write("Welcome to the Program! ");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your full name: ");
+        string Uname = Console.ReadLine();
+        return Uname;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number:");
+        int userInput = int.Parse(Console.ReadLine()); //when asking for an integer from the user
+        return userInput;
+    }
+    static int SquareNumber(int favoriteNumber)
+    {
+        int square = favoriteNumber * favoriteNumber;
+        return square;
+    }
+    static void DisplayResult(string userInput, int square)
+    {
+        Console.Write($"{userInput}, the square of your favorite number is {square}");
+    }
+
 }

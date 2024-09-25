b63241c57be9082539fc0a4501e051f13eea09b8
nanurodriguez <bel17039@byui.edu>
2024-09-25 01:21:24 -0600

    Finishing Step 5

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -16,5 +16,10 @@ public class Job
     public int _startYear;
     public int _endYear;
 
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
 }
 
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -12,8 +12,6 @@ class Program
         job1._startYear = 2020;
         job1._endYear = 2022;
 
-        Console.WriteLine($"{job1._jobTitle} ({job1._company}) {job1._startYear}-{job1._endYear}");
-
         Job job2 = new Job();
 
         job2._company = "Drop-Box";
@@ -21,6 +19,7 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2024;
 
-        Console.WriteLine($"{job2._jobTitle} ({job2._company}) {job2._startYear}-{job2._endYear}");
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
     }
 }

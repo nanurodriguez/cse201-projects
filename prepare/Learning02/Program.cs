550c039defba724a35071d1dbd23f0ed994a55a6
nanurodriguez <bel17039@byui.edu>
2024-09-25 01:07:58 -0600

    Completed Step 4 with another job example job2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -12,6 +12,15 @@ class Program
         job1._startYear = 2020;
         job1._endYear = 2022;
 
-        Console.WriteLine(job1._company);
+        Console.WriteLine($"{job1._jobTitle} ({job1._company}) {job1._startYear}-{job1._endYear}");
+
+        Job job2 = new Job();
+
+        job2._company = "Drop-Box";
+        job2._jobTitle = "Full Stack Developer";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Console.WriteLine($"{job2._jobTitle} ({job2._company}) {job2._startYear}-{job2._endYear}");
     }
 }

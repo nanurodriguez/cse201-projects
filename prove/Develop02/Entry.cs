281744b260f018c975aef39eb27bf650f6438a16
nanurodriguez <bel17039@byui.edu>
2024-09-27 02:25:05 -0600

    Finishing Entry Class- adding display

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
 using System.Reflection.Metadata.Ecma335;
 
 public class Entry
@@ -9,7 +10,48 @@ public class Entry
 
     public void Display()
     {
+        Journal journal = new Journal();
 
+        Console.WriteLine("Hello There! I am so glad you returned!");
+        Console.WriteLine("Welcome to YOUR Journal! ");
+
+        while (true)
+        {
+            //Menu Options given:
+
+            Console.WriteLine("1. Write: ");
+            Console.WriteLine("2. Display: ");
+            Console.WriteLine("3. Load: ");
+            Console.WriteLine("4. Save: ");
+            Console.WriteLine("5. Quit: ");
+
+            //If selected then:
+
+            string selection = Console.ReadLine();
+            float number = float.Parse(selection);
+
+            if (selection == "1")
+            {
+                journal.GetRandomPrompt();
+
+            }
+            else if (selection == "2")
+            {
+                journal.DisplayAll();
+            }
+            else if (selection == "3")
+            {
+                journal.LoadFromFile();
+            }
+            else if (selection == "4")
+            {
+                journal.SaveToFile();
+            }
+            else
+            {
+                Console.WriteLine("It's okay, Let's write in another moment!");
+            }
+        }
     }
 
 }
--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -24,30 +24,24 @@ public class Journal
     }
 
 
-    public void GetRandomPrompt(Entry newPrompt)
+    public void GetRandomPrompt()
     {
 
-        Entry prompt1 = new Entry();
-        prompt1._promptText = "What did you do today?";
-        Entry prompt2 = new Entry();
-        prompt2._promptText = "Did you meet someone?";
-        Entry prompt3 = new Entry();
-        prompt3._promptText = "Let's mention your blessings:";
-        Entry prompt4 = new Entry();
-        prompt4._promptText = "Did you find something interesting in your scripture reading?";
-        Entry prompt5 = new Entry();
-        prompt5._promptText = "Mention something you enjoyed doing today:";
-        Entry prompt6 = new Entry();
-        prompt6._promptText = "Did you learn something new?";
-
         PromptGenerator prompt = new PromptGenerator();
+        string[] strings = {
+            "What did you do today?", "Did you meet someone interesting?",
+         "Let's mention your blessings:", "Did you find something interesting in your scripture reading?",
+         "Mention something you enjoyed doing today:","Did you learn something new?".
+        };
 
-        newPrompt._promptText.Add(prompt1);
     }
 
     public void DisplayAll()
     {
-
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
     }
 
     public void SaveToFile(string file)

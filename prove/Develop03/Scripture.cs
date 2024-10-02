5b22b47e7f5850ace10e2e9f85e05427f1cfc1e8
nanurodriguez <bel17039@byui.edu>
2024-10-02 15:23:52 -0600

    Creating Scripture Memorizer

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,8 @@
+using System;
+
+//Scripture: Keeps track of both the reference and the text of the scripture. Can hide words and get the rendered display of the text.
+public class Scripture
+{
+    private string _reference;
+    private List<string> _words = new List<string>();
+}

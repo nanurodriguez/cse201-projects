9845b684c38f56c7135064d4e3413b2fe4fe54d5
nanurodriguez <bel17039@byui.edu>
2024-10-03 18:00:37 -0600

    Adding all the constructors and methods and Get and set methods to the Scripture class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -10,12 +10,16 @@ class Program
         Console.WriteLine("Testing Scripture Memorizer: ");
         Console.WriteLine(scripture.GetDisplayText());
 
+        //Calling the GetDisplayText to display first reference
         Reference reference = new Reference("Moses", 1, 39);
-        Console.WriteLine("Testing Reference...");
         Console.WriteLine(reference.GetDisplayText());
 
-        Reference reference2 = new Reference("Moses", 1, 39, 40);
-        Console.WriteLine("Testing Reference 2...");
+        //Calling the GetDisplayText2 to display second reference
+        Reference reference2 = new Reference("2 Nephi", 2, 6, 9);
         Console.WriteLine(reference2.GetDisplayText2());
+
+        //Calling the GetDisplayText to display third reference 
+        Reference reference3 = new Reference("2 Nephi", 32, 8);
+        Console.WriteLine(reference3.GetDisplayText());
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,16 +3,29 @@ using System;
 //Scripture: Keeps track of both the reference and the text of the scripture. Can hide words and get the rendered display of the text.
 public class Scripture
 {
-    //private string _reference;
+    private Reference _reference;
     public List<Word> _words = new List<Word>();
 
-    public Scripture()
+    //Constructor of Scripture with 2 parameters: Reference: Reference and text:string
+    public Scripture(Reference reference, string text)
     {
-
+        _reference = reference;
     }
 
+    public void HideRandomWords(int numberToHide)
+    {
+        //code to hide the random word selected Random
+    }
+
+    // GetDisplayText will make the scripture available for the user
     public string GetDisplayText()
     {
-        return "Moses 1:39";
+        return "GetDisplayText Class";
+    }
+    // IsCompletelyHidden class will help us know if the scripture was completerly hidden 
+    // so the code can quit
+    public bool IsCompletelyHidden()
+    {
+        return true;
     }
 }

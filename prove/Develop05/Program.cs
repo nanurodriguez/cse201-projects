0743ff1bc0a65a2fc2df62b7b0b12e9c8eb7cc29
nanurodriguez <bel17039@byui.edu>
2024-10-17 01:56:13 -0600

    Adding MENU

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,9 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        BreathingActivity activity1 = new BreathingActivity("Breathing Excercises", "Let's relax for a moment with this breathing excercise...", 20, "How many Seconds would you like to work on this?");
-        Console.WriteLine(activity1.DisplayStartingMessage());
+        while (true)
+        {
+            //Menu Options given:
 
+            Console.WriteLine("1. Breathing Activity: ");
+            Console.WriteLine("2. Listing Activity: ");
+            Console.WriteLine("3. Reflection Activity: ");
+            Console.WriteLine("4. Quit: ");
 
+            //If selected then:
+            Console.WriteLine("What would you like to do? ");
+            string selection = Console.ReadLine();
+            //float number = float.Parse(selection);
+
+            //adding switch for multiple options of selection
+
+            switch (selection)
+            {
+                case "1": //Displaying Content of BreathingActivity
+                    BreathingActivity breathingActivity = new BreathingActivity("Breathing Activity", "Description", 20, "Get Ready...");
+
+                    breathingActivity.Run();
+
+                    break;
+
+                case "2": // Display Content of ListingActivity
+                    ListingActivity listingActivity = new ListingActivity();
+                    //listingActivity.Run();
+                    break;
+
+                case "3": //Displaying Content of ReflectingActivity
+                    ReflectingActivity reflectingActivity = new ReflectingActivity();
+                    //listingActivity.Run();
+                    break;
+
+                case "4":
+                    Console.WriteLine("GoodBye!");
+                    break;
+                default: // will prompt the user to enter the correct option
+                    Console.WriteLine("Incorrect Entry! Please try again by selecting the options 1-5.");
+                    break;
+            }
+        }
     }
 }

5d239450f5e9db357d084498f8a128ac0b12fbfd
nanurodriguez <bel17039@byui.edu>
2024-10-22 11:30:59 -0600

    finishing circles class

--- prepare/Learning06/Circles.cs
+++ prepare/Learning06/Circles.cs
@@ -2,5 +2,20 @@
 
 public class Circles : Shapes
 {
+    private double _radius = 0;
 
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return 3.14 * (_radius * _radius);
+    }
 }
